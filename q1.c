10af686528133e05ca7b1542d6757466a7ce7194
Antonny Marcelo <166526832+antonnymarcelo133@users.noreply.github.com>
2024-07-14 17:12:11 -0300

    Add files via upload

---
+++ q1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+int tamanho;
+printf("Informe o tamanho do vetor: \t");
+scanf("%d",&tamanho);
+int * vetor = (int*) malloc(tamanho*sizeof(int));
+if(vetor==NULL){
+    printf("No Memory");
+    exit(1);
+}
+else{
+    printf("Vetor alocado com sucesso!");
+}
+
+printf("Digitar os elementos do vetor: \n ");
+for(int count=0; count<tamanho; count++){
+    scanf("%d", &vetor[count]);
+}
+int count;
+printf("Vetor inverso: \n");
+for(int count=tamanho-1; count>=0; count--){
+    printf("%d \t", vetor[count]);
+}
+free(vetor);
+
+    return(0);
+}
