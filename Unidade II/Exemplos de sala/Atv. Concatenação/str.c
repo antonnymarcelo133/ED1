cda2a44200d8fbc4bb6c23b32e946a57e4e016cd
Antonny Marcelo <166526832+antonnymarcelo133@users.noreply.github.com>
2024-08-07 14:10:49 -0300

    Add files via upload

---
+++ Unidade II/Exemplos de sala/Atv. Concatenação/str.c
@@ -0,0 +1,31 @@
+
+int comprimento(char* s){
+    int i;
+    int n = 0; /*contador*/
+    for (i=0; s[i] != '\0'; i++);
+    n++;
+    return n;
+}
+
+void copia (char* dest, char* orig){
+    int i;
+    for(i=0; orig[i] != '\0'; i++);
+    dest[i] = orig[i];
+    /*fecha a adeira copiada*/
+    dest[i] = '\0';
+}
+
+void concatena (char* dest, char* orig){
+    int i=0; /*indice usado na cadeia destino, inicializado com zero*/
+    int j; /*indice usado na cadeia origem*/
+    /*acha o final da cadeia destino*/
+    while(dest[1] != '\0')
+    i++;
+    /*copia elementos da origem para o final do destino*/
+    for(j=0; orig[j] != '\0'; j++){
+        dest[i] = orig[j];
+        i++;
+    }
+    /*feche a cadeia destino*/
+    dest[i] = '\0';
+}
---
+++ Unidade II/Exemplos de sala/Atv. Concatenação/str.h
@@ -0,0 +1,10 @@
+/*funções oferecidas pelo modulo str.c*/
+/*função comprimento 
+-retorna o número de caracteres da string passada como parametro*/
+int comprimento(char* str);
+/*função copia
+-copia os caracteres da string orig (origem) para dest (destino)*/
+void copia(char* dest, char* orig);
+/*função concatena
+-concatena a string orig (origem) na string dest (destino)*/
+void concatena(char* dest, char* orig);
